155394bed5eda50f237f8e3940473b1007970645
AmirAliMahmoud1 <39376240+AmirAliMahmoud1@users.noreply.github.com>
2020-06-23 11:28:15 +0200

    Add files via upload

---
+++ amir bot/commands/fun/meme.js
@@ -0,0 +1,24 @@
+const {  Discord } = require("discord.js");
+const randomPuppy = require("random-puppy");
+const { MessageEmbed } = require("discord.js");
+
+
+module.exports = {
+    name: "meme",
+    category: "fun",
+    description: "Sends an epic meme",
+    run: async (client, message, args) => {
+        const subReddits = ["dankmeme", "meme", "me_irl"];
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        const img = await randomPuppy(random);
+         const embed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            //.setTitle(`${random}`)
+			.setTitle(`From r/ ${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+        message.channel.send(embed);
+    }
+}
